ed6baad15df4a44d2fe8fe572f30abeebbd48ca3
shyrik13 <shyrik13@inbox.lv>
2022-03-03 23:28:44 +0200

    wasm opengl up

--- project/core/main/assets/js/wasm-opengl.js
+++ project/core/main/assets/js/wasm-opengl.js
@@ -1,3 +1,19 @@
 import * as wasmOpenGL from "wasm-opengl";
 
-wasmOpenGL.greet();
+var teaser_ctx = null;
+
+const tick_teaser = () => {
+    teaser_ctx.draw();
+    requestAnimationFrame(tick_teaser);
+}
+
+function start() {
+    if (teaser_ctx == null) {
+        teaser_ctx = wasmOpenGL.Context.new();
+    }
+    requestAnimationFrame(tick_teaser);
+}
+
+start();
+
+console.log("OpenGL");
